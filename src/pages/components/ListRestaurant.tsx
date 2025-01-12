6c7a347bf11d55995f287e3dbe7bc544be0bb51e
Nguyen Nguyen Nhat <nguyennhatnguyen1995@gmail.com>
2025-01-13 01:51:37 +0700

    Update code

--- src/pages/components/ListRestaurant.tsx
+++ src/pages/components/ListRestaurant.tsx
@@ -2,6 +2,10 @@ import { trpc } from "@/utils/trpc";
 import { ScrollArea } from "@radix-ui/react-scroll-area";
 import Image from "next/image";
 import React from "react";
+import StarsIcon from "@/assets/icons/starts.svg";
+import Star from "@/assets/icons/star-icon.svg";
+import Favorite from "@/assets/icons/favorite.svg";
+import { cn } from "@/lib/utils";
 
 type RestaurantItem = {
   id: string;
@@ -14,27 +18,71 @@ type RestaurantItem = {
   description: string;
   images: string[];
   priceRange: string;
-  featured: string | null;
+  featured: { text: string } | null;
   createdAt: string;
   updatedAt: string;
 };
 
-const ListRestaurantItem: React.FC<{ item: RestaurantItem }> = ({ item }) => {
+const ListRestaurantItem: React.FC<{
+  item: RestaurantItem;
+  refetchList: () => void;
+}> = ({ item, refetchList }) => {
+  const favoriteMutation = trpc.addFavorite.useMutation({
+    onSuccess: () => refetchList(),
+  });
+  const handleFavorite = async () => {
+    await favoriteMutation.mutateAsync({
+      id: item.id,
+      isFavorite: !item.isFavorite,
+    });
+  };
   return (
-    <div className="flex flex-col gap-4">
-      <div
-        style={{ width: 370, height: 200 }}
-        className="overflow-hidden rounded-xl"
-      >
-        <Image src={item.images[0]} alt={item.name} width={400} height={200} />
+    <div className="relative">
+      <Image
+        onClick={handleFavorite}
+        className={cn(
+          "absolute justify-end flex right-2 top-2",
+          item.isFavorite && "bg-red-400 rounded-full"
+        )}
+        src={Favorite}
+        alt="favorite-icon"
+      />
+
+      <div className="flex flex-col flex-wrap">
+        <div
+          style={{ width: 370, height: 200 }}
+          className="overflow-hidden rounded-xl"
+        >
+          <Image
+            src={item.images[0]}
+            alt={item.name}
+            width={400}
+            height={200}
+          />
+        </div>
+        {item.featured && (
+          <div className="mt-2 flex flex-row space-x-2">
+            <Image src={StarsIcon} alt="stars-icon" />
+            <span className="text-orange-400">{item.featured?.text}</span>
+          </div>
+        )}
+        <div className="flex flex-row justify-between flex-wrap">
+          <span className="font-bold line-clamp-1 w-3/4">{item.name}</span>
+          <div className="flex flow-row space-x-2 items-center">
+            <Image src={Star} alt="star-icon" />
+            <span className="text-sm ">
+              {item.rating} ({item.ratingCount})
+            </span>
+          </div>
+        </div>
+        <span className="mt-1">{item.description}</span>
       </div>
-      <span>{item.name}</span>
     </div>
   );
 };
 
 const ListRestaurant: React.FC = () => {
-  const { data, isLoading } = trpc.getRestaurants.useInfiniteQuery(
+  const { data, isLoading, refetch } = trpc.getRestaurants.useInfiniteQuery(
     {
       limit: 10,
     },
@@ -48,12 +96,13 @@ const ListRestaurant: React.FC = () => {
   }
   const dataShowing = data?.pages.map((page) => page?.restaurants).flat() ?? [];
 
-  console.log("data", dataShowing);
   return (
     <ScrollArea>
-      {dataShowing.map((item) => (
-        <ListRestaurantItem item={item} key={item.id} />
-      ))}
+      <div className="space-y-8">
+        {dataShowing.map((item) => (
+          <ListRestaurantItem item={item} key={item.id} refetchList={refetch} />
+        ))}
+      </div>
     </ScrollArea>
   );
 };
